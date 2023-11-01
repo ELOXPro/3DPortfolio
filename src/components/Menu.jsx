fc43e6a5c4969304ef12d3000fb40b37a87a26c4
ELOSTudios.inc <iradeloi41@gmail.com>
2023-11-01 19:09:24 +0200

    fixed Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,24 @@
 
 export const Menu = (props) => {
   const {onSectionChange, menuOpened, setMenuOpened} = props;
+
+  const handleClick = (e) => {
+    const label = e.target.innerText;
+    e.preventDefault();
+    setMenuOpened(false);
+    if (label === 'Introduction'){
+      onSectionChange(0);
+    } else if (label === 'Skills'){
+      onSectionChange(1);
+    } else if (label === 'About Me'){
+      onSectionChange(2);
+    } else if (label === 'Projects'){
+      onSectionChange(3);
+    } else if (label === 'Contact Me'){
+      onSectionChange(4);
+    }
+  }
+
   return (
     <>
     <button 
@@ -14,11 +32,11 @@ export const Menu = (props) => {
     <div className={`z-10 fixed top-0 right-0 bottom-0 bg-white transition-all overflow-hidden flex flex-col
       ${menuOpened ? "w-full md:w-80" : "w-0"}`}>
       <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
-        <MenuButton label= "Introduction" onClick={()=> onSectionChange(0)}/>
-        <MenuButton label= "Skills" onClick={()=> onSectionChange(1)}/>
-        <MenuButton label= "About Me" onClick={()=> onSectionChange(2)}/>
-        <MenuButton label= "Projects" onClick={()=> onSectionChange(3)}/>
-        <MenuButton label= "Contact Me" onClick={()=> onSectionChange(4)}/>
+        <MenuButton label= "Introduction" onClick={handleClick}/>
+        <MenuButton label= "Skills" onClick={handleClick}/>
+        <MenuButton label= "About Me" onClick={handleClick}/>
+        <MenuButton label= "Projects" onClick={handleClick}/>
+        <MenuButton label= "Contact Me" onClick={handleClick}/>
       </div>
     </div>
     </>
@@ -28,14 +46,9 @@ export const Menu = (props) => {
 const MenuButton = (props) => {
   const {label, onClick} = props;
 
-  const handleClick =()=>{
-    setMenuOpened(!menuOpened);
-    onClick ={onClick}
-  }
-
   return (
     <button
-      onClick ={handleClick}
+      onClick ={onClick}
       className="text-2xl font-bold cursor-pointer hover:text-blue-500 text-blue-700 transition-colors">
       {label}
     </button>
